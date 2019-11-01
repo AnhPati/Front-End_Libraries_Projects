08579eb27ee2737bdec97194334c556ce88e6524
AnhPati <Romain-felicianne@hotmail.fr>
2019-11-01 21:12:30 +0100

    Styles PreviewBox
    Ajout du fichier PreviewBox.css qui gère les styles du composant du même nom.
    Ajouts de classes Bootstrap sur :
    - Le conteneur principal du composant PreviewBox.
    - Le titre du composant.
    - Le paragraphe de rendu.
    - Le paragraphe généré par la métode handleRead().

--- build_a_markdown_previewer/src/component/PreviewBox.js
+++ build_a_markdown_previewer/src/component/PreviewBox.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
+import '../style/css/PreviewBox.css';
 
 class PreviewBox extends Component {
     render() {
         return(
-            <div>
-                Preview
-                <div dangerouslySetInnerHTML={{__html: this.props.renderText}}/>
+            <div className="w-100 px-5 previewBox-container">
+                <h6 className="mt-2 ml-2">Preview</h6>
+                <p
+                    className="w-100 previewBox-item mb-4 px-2"
+                    dangerouslySetInnerHTML={{__html: this.props.renderText}}
+                />
             </div>
         )
     }
