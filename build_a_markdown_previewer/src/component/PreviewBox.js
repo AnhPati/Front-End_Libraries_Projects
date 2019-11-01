2911b6cc8a9aee0ce3f0952dbeeb919e9b3adc27
AnhPati <Romain-felicianne@hotmail.fr>
2019-11-01 22:08:03 +0100

    Collapse PreviewBox
    Ajout d'une fonction collapse sur le paragraphe de rendu à l'aide de Bootstrap.
    Ajout de classes Bootstrap pour corriger les rendus des liens par défault.

--- build_a_markdown_previewer/src/component/PreviewBox.js
+++ build_a_markdown_previewer/src/component/PreviewBox.js
@@ -4,10 +4,20 @@ import '../style/css/PreviewBox.css';
 class PreviewBox extends Component {
     render() {
         return(
-            <div className="w-100 px-5 previewBox-container">
-                <h6 className="mt-2 ml-2">Preview</h6>
+            <div className="w-100 px-5 previewBox-container" id="previewBox-collapse">
+                <a
+                    className=""
+                    role="button"
+                    data-toggle="collapse"
+                    href="#previewItem-collapse"
+                    aria-expanded="false"
+                    aria-controls="previewItem-collapse"
+                >
+                    <h6 className="mt-2 ml-2">Preview</h6>
+                </a>
                 <p
-                    className="w-100 previewBox-item mb-4 px-2"
+                    className="w-100 previewBox-item mb-4 px-2 collapse show"
+                    id="previewItem-collapse"
                     dangerouslySetInnerHTML={{__html: this.props.renderText}}
                 />
             </div>
