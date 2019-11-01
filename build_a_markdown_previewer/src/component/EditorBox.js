ab47658363690ecb3ed73c64d43fd0c6cfcfa3bd
AnhPati <Romain-felicianne@hotmail.fr>
2019-11-01 20:42:28 +0100

    Styles + fonts EditorBox
    Ajout du fichier EditorBox.css qui gère les styles du composant du même nom.
    Ajouts de classes Bootstrap sur :
    - Le conteneur du composant EditorBox,
    - Le textearea du composant EditorBox.
    Ajout des fonts Latina Essentiel.

--- build_a_markdown_previewer/src/component/EditorBox.js
+++ build_a_markdown_previewer/src/component/EditorBox.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
+import '../style/css/EditorBox.css'
 
 class EditorBox extends Component {
     render() {
         return(
-            <div>
-                Editor
-                <input 
+            <div className="w-100 px-5 editorBox-container">
+                <h6 className="mt-2 ml-2">Editor</h6>
+                <textarea
+                    className="w-100 editorBox-item mb-4 px-2" 
                     type="text"
                     value={this.props.originText} 
                     onChange={this.props.handleChange}
