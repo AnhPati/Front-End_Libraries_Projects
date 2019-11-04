3854a530b16d304cffd2f66531590bdf884e5b17
AnhPati <Romain-felicianne@hotmail.fr>
2019-11-04 07:43:40 +0100

    Switch Text/HTML
    Ajout d'une méthode handleRender() sur le composant Markdownpreviewer.
    Cette méthode est passé en props au composant PreviewBox et sert à switcher entre la vue HTML et le texte préformaté.
    Pour ce faire ajout d'un nouveau state "renderHTML", un booléen qui permet d'afficher ou non le texte en HTML pur.
    Ajout de styles sur le bouton associé.

--- build_a_markdown_previewer/src/component/MarkdownPreviewer.js
+++ build_a_markdown_previewer/src/component/MarkdownPreviewer.js
@@ -10,10 +10,12 @@ class MarkdowPreviewer extends Component {
         this.state = {
             originText: '',
             renderText: '',
+            renderHTML: true,
         }
 
         this.handleChange = this.handleChange.bind(this);
         this.handleRead = this.handleRead.bind(this);
+        this.handleRender = this.handleRender.bind(this);
     }
     
     handleChange = (event) => {
@@ -29,16 +31,26 @@ class MarkdowPreviewer extends Component {
         this.setState ({
             originText: originText,
             renderText: htmlText,
+            renderHTML: this.state.renderHTML,
         });
+    }
+
+    handleRender = () => {
+        console.log(this.state);
+        this.setState({
+            originText: this.state.originText,
+            renderText: this.state.renderText,
+            renderHTML: !this.state.renderHTML,
+        })
     }
 
     render() {
         
         return(
             <div className="d-flex flex-column align-items-center p-5 min-vh-100 markdownPreviewer-container">
-                <h1 className="mb-5">Markdown <span class="text-light">Previewer</span></h1>
+                <h1 className="mb-5">Markdown <span className="text-light">Previewer</span></h1>
                 <EditorBox handleChange={this.handleChange} originText={this.state.originText}/>
-                <PreviewBox renderText={this.state.renderText}/>
+                <PreviewBox handleRender={this.handleRender} renderHTML={this.state.renderHTML} renderText={this.state.renderText}/>
             </div>
         )
     }
--- build_a_markdown_previewer/src/component/PreviewBox.js
+++ build_a_markdown_previewer/src/component/PreviewBox.js
@@ -5,21 +5,38 @@ class PreviewBox extends Component {
     render() {
         return(
             <div className="w-100 px-5 previewBox-container" id="previewBox-collapse">
-                <a
-                    className=""
-                    role="button"
-                    data-toggle="collapse"
-                    href="#previewItem-collapse"
-                    aria-expanded="false"
-                    aria-controls="previewItem-collapse"
-                >
-                    <h6 className="mt-2 ml-2">Preview</h6>
-                </a>
-                <p
-                    className="w-100 previewBox-item mb-4 px-2 collapse show"
-                    id="previewItem-collapse"
-                    dangerouslySetInnerHTML={{__html: this.props.renderText}}
-                />
+                <div className="d-flex justify-content-between">
+                    <a
+                        className=""
+                        role="button"
+                        data-toggle="collapse"
+                        href="#previewItem-collapse"
+                        aria-expanded="false"
+                        aria-controls="previewItem-collapse"
+                    >
+                        <h6 className="mt-2 ml-2">Preview</h6>
+                    </a>
+                    <h6
+                        className="mt-2 ml-2 renderHTML-btn"
+                        onClick={this.props.handleRender}
+                    >
+                        {this.props.renderHTML ? "HTML view" : "Text view"}
+                    </h6>
+                </div>
+                {this.props.renderHTML ? (
+                    <p
+                        className="w-100 previewBox-item mb-4 px-2 collapse show renderHTML-item"
+                        id="previewItem-collapse"
+                    >
+                        {this.props.renderText.substring(3, this.props.renderText.length - 5)}
+                    </p>
+                ) : (
+                    <p
+                        className="w-100 previewBox-item mb-4 px-2 collapse show"
+                        id="previewItem-collapse"
+                        dangerouslySetInnerHTML={{__html: this.props.renderText}}
+                    />
+                )}
             </div>
         )
     }
